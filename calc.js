81b91ae073859644bc495f2b9482d693e06532c9
shimizukobo <43zutaka32@gmail.com>
2025-09-06 17:12:02 +0900

    Update calc.js

--- calc.js
+++ calc.js
@@ -40,28 +40,28 @@ export class CalcVR {
             this.objectSize = '1.0 1.0 1.0';
             this.newDistance = 800;
         }else if(distance > 1000 && distance <= 8000) {
-//            this.objectSize = '20 20 20';
+            this.objectSize = '20 20 20';
 //            this.objectSize = '2.0 2.0 2.0';
 //            this.objectSize = '1.0 1.0 1.0';
-            this.objectSize = '0.9 0.9 0.9';
+//            this.objectSize = '0.9 0.9 0.9';
             this.newDistance = 800 + (distance/1000);
         }else if(distance > 8000 && distance <= 16000) {
-//            this.objectSize = '18 18 18';
+            this.objectSize = '18 18 18';
 //            this.objectSize = '1.8 1.8 1.8';
 //            this.objectSize = '0.9 0.9 0.9';
-            this.objectSize = '0.8 0.8 0.8';
+//            this.objectSize = '0.8 0.8 0.8';
             this.newDistance = 800 + (distance/1000);
         }else if(distance > 16000 && distance <= 20000) {
-//            this.objectSize = '15 15 15';
+            this.objectSize = '15 15 15';
 //            this.objectSize = '1.5 1.5 1.5';
 //            this.objectSize = '0.7 0.7 0.7';
-            this.objectSize = '0.6 0.6 0.6';
+//            this.objectSize = '0.6 0.6 0.6';
             this.newDistance = 800 + (distance/1000);
         }else if(distance > 20000) {
-//            this.objectSize = '10 10 10';
+            this.objectSize = '10 10 10';
 //            this.objectSize = '1 1 1';
 //            this.objectSize = '0.5 0.5 0.5';
-            this.objectSize = '0.3 0.3 0.3';
+//            this.objectSize = '0.3 0.3 0.3';
             this.newDistance = 800 + (distance/1000);
         }
     }
